edc19d7b6a7cec660b4b572d6fca1cb9fad84eb0
Lnvictor <Lnvictor@github.com>
2020-04-18 14:12:34 -0300

    Landing Page Submission
    close #49

--- pypro/base/tests/test_home.py
+++ pypro/base/tests/test_home.py
@@ -1,6 +1,22 @@
+import pytest
 from django.test import Client
+from django.urls import reverse
+
+from pypro.django_assertions import assert_contains
 
 
-def test_home(client: Client):
-    resp = client.get('/')
+@pytest.fixture
+def resp(client: Client):
+    return client.get(reverse('base:home'))
+
+
+def test_home(resp):
     assert resp.status_code == 200
+
+
+def test_title(resp):
+    assert_contains(resp, '<title>Python Pro</title>')
+
+
+def test_home_link(resp):
+    assert_contains(resp, f'href="{reverse("base:home")}">Python Pro</a>')

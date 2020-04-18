f3c00bf08c48b9d12d766368d71c62e86fd6071a
Lnvictor <Lnvictor@github.com>
2020-04-18 17:32:04 -0300

    Landing Page -- Entrega

--- pypro/base/tests/test_home.py
+++ pypro/base/tests/test_home.py
@@ -5,7 +5,8 @@ from pypro.django_assertions import assert_contains
 
 @pytest.fixture
 def resp(client):
-    return client.get(reverse('base:home'))
+    resp = client.get(reverse('base:home'))
+    return resp
 
 
 def test_home(resp):

4eddff428a8202ba74679b56b0827e6d868577a9
Lnvictor <Lnvictor@github.com>
2020-04-18 15:33:56 -0300

    Attempt

--- pypro/base/tests/test_home.py
+++ pypro/base/tests/test_home.py
@@ -5,8 +5,8 @@ from pypro.django_assertions import assert_contains
 
 @pytest.fixture
 def resp(client):
-    # return client.get(reverse('base:home'))
-    return 200
+    return client.get(reverse('base:home'))
+
 
 def test_home(resp):
     assert resp.status_code == 200

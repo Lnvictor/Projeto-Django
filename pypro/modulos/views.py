e0b99199c7eeb2adcae0bfd8725f51447cb435fe
Lnvictor <Lnvictor@github.com>
2020-05-07 11:33:44 -0300

    Criada a Página de Detalhamento de Aulas

--- pypro/modulos/views.py
+++ pypro/modulos/views.py
@@ -3,6 +3,18 @@ from django.shortcuts import render
 from pypro.modulos import facade
 
 
+def indice(request):
+    contexto = {'modulos': facade.listar_modulos_com_aulas()}
+    return render(request, 'modulos/indice.html', contexto)
+
+
 def detalhe(request, slug):
     modulo = facade.encontrar_modulo(slug)
-    return render(request, 'modulos/modulo_detalhe.html', {'modulo': modulo})
+    aulas = facade.listar_aulas_de_modulo_ordenadas(modulo)
+    return render(request, 'modulos/modulo_detalhe.html', {'modulo': modulo, 'aulas': aulas})
+
+
+def aula(request, slug):
+    aula = facade.encontrar_aula(slug)
+    return render(request, 'modulos/aula_detalhe.html', {'aula': aula})
+
